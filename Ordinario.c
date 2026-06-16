38ac63b18775a69b1d994e5fdbabf0b6062bc0d3
Mattixs <matiasubl10@gmail.com>
2026-06-16 09:09:44 -0600

    Add Ordinario.c with main function and structures
    Main del código.

---
+++ Ordinario.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+#include "altas.h"
+
+struct Persona{
+    char *nombre;
+    int edad;
+    char genero; 
+    char fecha[8];
+    struct Alumno *PtrAl; 
+    struct Persona *Ptrsig; 
+};
+
+struct  Alumno{
+    char matricula[10];
+    int carrera;
+    int semestre;
+    char correo[13]; 
+    float calif[5][5]; 
+}; 
+
+int main(){
+    int opcion;
+    struct Persona *Ptr;
+
+    do{
+        opcion = menuPrincipal();
+        switch (opcion){
+        case 1:
+            altas(&Ptr);
+            break;
+        case 2:
+            break; 
+        case 3: 
+            break; 
+        case 4: 
+            break; 
+        default:
+            break;
+        }
+    }while(opcion!=5);
+
+    return 0;
+}
