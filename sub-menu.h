96731c0ad812bea65d55b4f4e69adc4a56634489
Mattixs <matiasubl10@gmail.com>
2026-06-21 21:14:59 -0600

    Rename and update sub-menu function for careers

--- sub-menu.h
+++ sub-menu.h
@@ -1,16 +1,20 @@
 
 #include <stdio.h>
 
-int subMenu(void);
+int menuCarreras(void);
 
-int subMenu(void){
+int menuCarreas(void){
+  int opcion; 
   printf("\nCARRERAS;\n"); 
+  
   printf("1 - Licenciatura en Ciencias Ambientales");
   printf("2 - Ingeniería Forestal"); 
   printf("3 - Ingeniería en Desarrollo de Software");
   printf("4 - Licenciatura en Biología");
   printf("5 - Licenciatura en Administración Turistíca");
   printf("Seleccione su carrera: ");
-  scanf("%d", &A -> carrera); 
+  scanf("%d", &opcion); 
+
+  return opcion; 
   
 }
