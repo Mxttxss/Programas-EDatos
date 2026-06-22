713f7ad8975b188cc3a3ab1d88203c247a88c205
Mattixs <matiasubl10@gmail.com>
2026-06-22 09:40:47 -0600

    Refactor altas.h to improve structure and memory allocation

--- altas.h
+++ altas.h
@@ -1,48 +1,79 @@
 
+#ifndef STRUCT_H
+#define STRUCT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Persona *PtrP;
-struct Alumno *PtrA;
+struct Persona *nuevaPersona();
+struct Alumno *nuevoAlumno();
 
 int altas(struct Persona **Ptr){
-    struct Persona *PtrAux;
+    struct Persona *P = NULL; 
+    struct Alumno *A = NULL; 
 
-    struct Persona nuevaPersona(){
-        struct Persona *P;
-        P = (struct Persona *)malloc(sizeof(struct Persona));
-        return P;
+    P = nuevaPersona();
+
+    if(P == NULL){
+        printf("Error, no se pudo crear :(");
+        return 0; 
     }
 
-    struct Alumno nuevoAlumno(){
-        struct Alumno *A;
-        A = (struct Alumno *)malloc(sizeof(struct Alumno)); 
-        return A;
-    }
-
-printf("\nPERSONA\n"); 
-printf("Nombre: ");
-scanf("%s", P -> nombre); 
-printf("Edad: ");
-scanf("%d", &P -> edad); +
-printf("Género: ");
-scanf("%c", &P -> genero);
-printf("Fecha de nacimiento: [DD/MM/AAAA] ");
-scanf("%s", P -> fecha); 
-
-printf("\nALUMNO\n");
-printf("Matricula: "); 
-scanf("%s", A -> matricula);
-printf("Carrera: ");
-scanf("%d", &A -> carrera); 
-printf("Correo: "); 
-
-for(int i=0; i<5; i++){
-    for(int j=0; j<5; j++{
-        printf("Calificaciones [%d][%d]: ", i, j); 
-        scanf("%f", &A -> calif[i][j]); 
-    }
-}
+    A = nuevoAlumno();
     
+    if(A == NULL){
+       free(P);
+       return 0; 
+    }
+    
+    P->PtrAl = A; 
+    P->PtrSig = *Ptr;
+    *Ptr = P; 
+
+    return 1; 
+}
+
+struct Persona *nuevaPersona(){
+    struct Persona *P;
+
+        P = (struct Persona *)malloc(sizeof(struct Persona));
+        if (P == NULL){
+            return NULL; 
+        }
+
+        printf("\nPERSONA\n"); 
+        printf("Nombre: ");
+        scanf("%s", P -> nombre); 
+        printf("Edad: ");
+        scanf("%d", &P -> edad); 
+        printf("Género: ");
+        scanf("%c", &P -> genero);
+        printf("Fecha de nacimiento: [DD/MM/AAAA] ");
+        scanf("%s", P -> fecha); 
+        
+        return P;
+    } 
+
+struct Alumno nuevoAlumno(){
+    struct Alumno *A;
+
+        A = (struct Alumno *)malloc(sizeof(struct Alumno)); 
+        if (A == NULL)
+            return NULL; 
+
+        printf("\nALUMNO\n");
+        printf("Matricula: "); 
+        scanf("%s", A -> matricula);
+        printf("Carrera: ");
+        scanf("%d", &A -> carrera); 
+        printf("Correo: "); 
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                printf("Calificaciones [%d][%d]: ", i, j); 
+                scanf("%f", &A -> calif[i][j]); 
+            }
+        }
 
 }
+
+#endif
