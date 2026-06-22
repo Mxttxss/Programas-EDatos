193b0469a8ff4867f1aad38cda9bbf4b665d6737
Mattixs <matiasubl10@gmail.com>
2026-06-21 21:09:52 -0600

    Create sub-menu.h

---
+++ sub-menu.h
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int subMenu(void);
+
+int subMenu(void){
+  printf("\nCARRERAS;\n"); 
+  printf("1 - Licenciatura en Ciencias Ambientales");
+  printf("2 - Ingeniería Forestal"); 
+  printf("3 - Ingeniería en Desarrollo de Software");
+  printf("4 - Licenciatura en Biología");
+  printf("5 - Licenciatura en Administración Turistíca");
+  printf("Seleccione su carrera: ");
+  scanf("%d", &A -> carrera); 
+  
+}
