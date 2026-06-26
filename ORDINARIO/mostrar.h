da6895764ea6b28f1b5064ae0b22ffde8119a64a
Mattixs <matiasubl10@gmail.com>
2026-06-26 15:59:28 -0600

    Add mostrar.h for displaying Persona and Alumno details
    This file contains functions to display information about 'Persona' and 'Alumno' structures, including filtering by career, semester, name, and matricula.

---
+++ ORDINARIO/mostrar.h
@@ -0,0 +1,122 @@
+
+include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "structs.h"
+#include "menu.h"
+
+void mostrar(struct Persona *Aux){
+    printf("\nPERSONA");
+    printf("\nNombre: %s", Aux->nombre);
+    printf("\nEdad: %d", Aux->edad);
+    printf("\nGenero: %c", Aux->genero);
+    printf("\nFecha: %s", Aux->fecha);
+
+
+    printf("\nALUMNO");
+    printf("\nMatricula: %s", Aux->PtrAl->matricula);
+    printf("\nCarrera: %d", Aux->PtrAl->carrera);
+    printf("\nSemestre: %d", Aux->PtrAl->semestre);
+    printf("\nCorreo: %s@unsij.edu.mx", Aux->PtrAl->correo);
+    printf("\n");
+}
+
+int mostrarTodos(struct Persona *Ptr){
+    while(Ptr!=NULL){
+        mostrar(Ptr);
+        Ptr=Ptr->PtrSig;
+    }
+    return 0;
+}
+
+
+
+int mostrarCarrera(struct Persona *Ptr){
+    int carrera;
+    carrera = menuCarreras();
+    while(Ptr!=NULL){
+        if(Ptr->PtrAl->carrera == carrera){
+            mostrar(Ptr);
+        }
+        Ptr=Ptr->PtrSig;
+    }
+    return 0;
+}
+
+int mostrarSemestre(struct Persona *Ptr){
+    int semestre;
+    printf("\nSemestre: ");
+    scanf("%d",&semestre);
+    while(Ptr!=NULL){
+        if(Ptr->PtrAl->semestre == semestre){
+            mostrar(Ptr);
+        }
+        Ptr=Ptr->PtrSig;
+    }
+
+    return 0;
+}
+
+int mostrarCarrerasemestre(struct Persona *Ptr){
+    int carrera;
+    int semestre;
+    carrera = menuCarreras();
+
+    printf("\nSemestre: ");
+    scanf("%d",&semestre);
+    while(Ptr!=NULL){
+        if(
+            Ptr->PtrAl->carrera == carrera && Ptr->PtrAl->semestre == semestre){
+            mostrar(Ptr);
+        }
+        Ptr=Ptr->PtrSig;
+    }
+
+    return 0;
+}
+
+int mostrarNombre(struct Persona *Ptr){
+    char nombre[50];
+    int nom;
+    printf("\nNombre: ");
+    scanf(" ");
+    fgets(nombre,50,stdin);
+
+    while(Ptr != NULL){
+        nom = 1;
+        for(int i=0; i<50; i++){
+            if(nombre[i] != Ptr->nombre[i]){
+                nom = 0;
+            }
+        }
+        if(nom == 1){
+            mostrarTodos(Ptr);
+        }
+        Ptr = Ptr->PtrSig;
+    }
+
+    return 0;
+}
+
+int mostrarMatricula(struct Persona *Ptr){
+    char matricula[10];
+    int dato;
+    printf("\nMatricula: ");
+    scanf("%s", matricula);
+
+    while(Ptr != NULL){
+        dato = 1;
+        for(int i=0; i<10; i++){
+            if(matricula[i] != Ptr->PtrAl->matricula[i]){
+                dato = 0;
+            }
+        }
+        if(dato == 1){
+            mostrarTodos(Ptr);
+        }
+
+        Ptr = Ptr->PtrSig;
+    }
+
+    return 0;
+}
